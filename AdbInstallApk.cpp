0ba749c1ed699b8961ad03b27044aa179a2fe395
csabikaa97 <csabikaa97@gmail.com>
2017-03-21 18:33:33 +0100

    Update

--- AdbInstallApk.cpp
+++ AdbInstallApk.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
+#include <stdlib.h>
 #include <cstdlib>
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-	string AdbInstall="adb insall ";
-    string command;
+	string AdbInstall="adb install ";
+    	string command;
 	command=AdbInstall+argv[1];
 	system(command.c_str());
+	return 0;
 }
