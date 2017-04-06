42461e48bc64cd3fa2b0923a61cc950ac09a117b
csabikaa97 <csabikaa97@gmail.com>
2017-04-06 17:09:39 +0200

    updated the source

--- SetBrightness.cpp
+++ SetBrightness.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 #include <cstdlib>
+#include <fstream>
 using namespace std;
 int main(int argc, char* argv[])
 {
 	if(argc==2)
 	{
+		ifstream file;
+		file.open("/sys/class/backlight/acpi_video0/brightness");
+		if(!file.is_open())
+		{
+			cout<<endl<<"This computer doesn't support changing brghtness."<<endl;
+			return 0;
+		}
 		string command1 =" sudo echo \"";
 		string command2 ="\" > /sys/class/backlight/acpi_video0/brightness";
 		string final=command1+argv[1]+command2;
