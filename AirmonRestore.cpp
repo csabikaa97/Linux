049e4fb67dde2176539e6f57bd4c35301f909f6c
csabikaa97 <csabikaa97@gmail.com>
2017-08-27 18:29:54 +0200

    readme file fuse and changes

--- AirmonRestore.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <cstdlib>
-using namespace std;
-int main()
-{
-	system("sudo service wpa_supplicant restart");
-	system("sudo service NetworkManager restart");
-	return 0;
-}
--- Rmjunk.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <cstdlib>
-using namespace std;
-int main()
-{
-	system("sudo rm *.csv");
-	system("sudo rm *.netxml");
-	return 0;
-}
