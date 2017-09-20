75ad04a5c137dfd3975444c928608a5b9c6a62a2
csabikaa97 <csabikaa97@gmail.com>
2017-09-20 21:57:21 +0200

    Update Spammer.py

--- Spammer.py
+++ Spammer.py
@@ -9,6 +9,7 @@ Times=10
 Essidbase="NULL"
 Filepath="NULL"
 i=0
+print("Argument initialization start...")
 while i<len(sys.argv):
 	print("argv["+str(i)+"]: "+sys.argv[i])
 	if sys.argv[i]=="c":
@@ -19,7 +20,52 @@ while i<len(sys.argv):
 		Interface=sys.argv[i+1]
 		print("Found interface: "+Interface)
 		i=i+1
-	if sys.argv[i]=="":
-		
+	if sys.argv[i]=="e":
+		Essid=sys.argv[i+1]
+		print("Essid found: "+Essid)
+		i=i+1
+	if sys.argv[i]=="p":
+		Precharacters=sys.argv[i+1]
+		print("Prefix characters found: "+Precharacters)
+		i=i+1
+	if sys.argv[i]=="f":
+		Filepath=sys.argv[i+1]
+		i=i+1
+	if sys.argv[i]=="b":
+		Bssid=sys.argv[i+1]
+		print("Bssid found: "+Bssid)
+		i=i+1
+	if sys.argv[i]=="t":
+		Times=int(sys.argv[i+1])
+		i=i+1
+	if sys.argv[i]=="l":
+		Essidbase=sys.argv[i+1]
+		print("Essid base found: "+Essidbase)
 		i=i+1
 	i=i+1
+print("Argument initialization done.")
+print("Checking wireless devices...")
+if not Interface == "NULL":
+	try:
+		InterfaceFile=open("/sys/class/net/"+str(Interface)+"/type","r")
+	except IOError:
+		print("Interface "+Interface+" is not available.")
+		return 0
+	print("Interface "+Interface+" is up.")
+	try:
+		InterfaceFile=open("/sys/class/net/mon0/type")
+	except IOError:
+		print("mon0 interface is not up")
+		print("Starting airmon-ng")
+		os.system("airmon-ng check kill")
+		os.system("airmon-ng start "+str(Interface))
+	try:
+		InterfaceFile=open("/sys/class/net/"+Interface"+/type")
+		if InterfaceFile.read() == "803":
+			print("Interface "+Interface+" is in monitor mode.")
+		else:
+			print("Interface "+Interface+"is not in monitor mode. Changing it...")
+			os.system("iwconfig "+Interface+" mode monitor")
+	except IOError:
+		return 0
+print("Checking wireless devices done.")
