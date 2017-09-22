c48874e136f85bb1f05306ea0f8b5ff2824e077b
csabikaa97 <csabikaa97@gmail.com>
2017-09-22 13:26:14 +0200

    Update

--- Spammer.py
+++ Spammer.py
@@ -9,6 +9,8 @@ Bssid="NULL"
 Times=10
 Essidbase="NULL"
 Filepath="NULL"
+Frequency=1;
+Fixchannel=0;
 i=0
 print("Argument initialization start...")
 while i<len(sys.argv):
@@ -16,6 +18,7 @@ while i<len(sys.argv):
 	if sys.argv[i]=="c":
 		Channel=sys.argv[i+1]
 		print("Found channel: "+Channel)
+		Fixchannel=1
 		i=i+1
 	if sys.argv[i]=="i":
 		Interface=sys.argv[i+1]
@@ -31,6 +34,7 @@ while i<len(sys.argv):
 		i=i+1
 	if sys.argv[i]=="f":
 		Filepath=sys.argv[i+1]
+		print("List file found: "+Filepath)
 		i=i+1
 	if sys.argv[i]=="b":
 		Bssid=sys.argv[i+1]
@@ -38,10 +42,15 @@ while i<len(sys.argv):
 		i=i+1
 	if sys.argv[i]=="t":
 		Times=int(sys.argv[i+1])
+		print("Runs "+str(Times)+" times.")
 		i=i+1
 	if sys.argv[i]=="l":
 		Essidbase=sys.argv[i+1]
 		print("Essid base found: "+Essidbase)
+		i=i+1
+	if sys.argv[i]=="T":
+		Frequency=int(sys.argv[i+1])
+		print("Frequency found: "+str(Frequency))
 		i=i+1
 	i=i+1
 print("Argument initialization done.")
@@ -74,30 +83,32 @@ else:
 	sys.exit()
 print("Checking wireless devices done.")
 print("Starting generation...")
-for i in range(1,Times):
-	if Channel == "NULL":
+Command=""
+for i in range(0,Times):
+	if Fixchannel == 0:
 		Channel=str(randint(1,11))
-	if Essid == "NULL":
-		if not Filepath == "NULL":
-			try:
-				Essidlistfile=open(str(Filepath))
-			except IOError:
-				print("Can't open list file.")
-				sys.exit()
-			EssidlistfileString=Essidlistfile.read()
-		if not Essidbase == "NULL":
-			Essid=Essidbase+"-"+str(randint(1,9999999))
-		if not Precharacters == "NULL":
-			for i in range(1,len(Precharacters)):
-				Essid=Essid+
-	if i == 1:
-		Basecommand="airbase-ng -c "+Channel+" -e "+Essid+" -a "+Bssid+" mon0"
-		print("Basecommand: "+Basecommand)
+	if not Filepath == "NULL":
+		try:
+			Essidlistfile=open(str(Filepath))
+		except IOError:
+			print("Can't open list file.")
+			sys.exit()
+		EssidlistfileString=Essidlistfile.read()
+	if not Essidbase == "NULL":
+		Essid=Essidbase+"-"+str(randint(1,9999999))
+	if i == 0:
+		Command="airbase-ng -c "+Channel+" -e "+Essid+" -a "+Bssid+" mon0"
 	else:
+		Command=Command+" && SleepnRun \"airbase-ng -c "+Channel+" -a "+ Bssid+" -e "+Essid+" mon0\" "+str(i+1)
 print("Generation done.")
+print("Command: \""+Command+"\"")
 print("Running script(s)...")
+os.system(str(Command))
+os.system(str(Command))
 
 print("Closing program...")
+os.system("airmon-ng stop mon0")
 os.system("killall SleepnRun")
 os.system("killall airbase-ng")
+os.system("iwconfig "+Interface+" mode managed")
 print("Thanks for using Spammer :D")
