0b78a288aad80cf34609801c7da502de3ea5ae09
csabikaa97 <csabikaa97@gmail.com>
2018-01-16 21:34:29 +0100

    Update AdbControl.cpp

--- AdbControl.cpp
+++ AdbControl.cpp
@@ -42,8 +42,87 @@ int main() {
             endwin();
             return 0;
         }
-	switch(input) {
-
+	if(input==105) {
+            cout<<endl<<"Szöveg: ";
+            cin>>text;
+            text=b+text;
+            system(text.c_str());
+        }
+        if(input==112) {
+            system("adb shell input keyevent KEYCODE_POWER");
+        }
+        if(input==116)
+        {
+            cout<<endl<<"APK helye: ";
+            cin>>TempPATH;
+            string command2="adb install "+TempPATH;
+            system(command2.c_str());
+            cout<<endl;
+        }
+        if(input==83)
+        {
+            cout<<endl;
+            system("adb shell");
+        }
+        if(input==104)
+        {
+            system("adb shell input keyevent KEYCODE_HOME");
+        }
+        if(input==98)
+        {
+            system("adb shell input keyevent KEYCODE_BACK");
+        }
+        if(input==72)
+        {
+            system("adb shell input keyevent KEYCODE_DPAD_UP");
+        }
+        if(input==80)
+        {
+            system("adb shell input keyevent KEYCODE_DPAD_DOWN");
+        }
+        if(input==75)
+        {
+            system("adb shell input keyevent KEYCODE_DPAD_LEFT");
+        }
+        if(input==77)
+        {
+            system("adb shell input keyevent KEYCODE_DPAD_RIGHT");
+        }
+        if(input==13)
+        {
+            system("adb shell input keyevent KEYCODE_ENTER");
+        }
+        if(input==117)
+        {
+            system("adb shell input keyevent 66");
+        }
+        if(input==110)
+        {
+            system("adb shell input swipe 360 1 360 1280");
+        }
+        if(input==119)
+        {
+            system("adb shell input swipe 360 1279 360 1");
+        }
+        if(input==97)
+        {
+            system("adb shell input swipe 1 640 719 640");
+        }
+        if(input==115)
+        {
+            system("adb shell input swipe 719 1 719 1279");
+        }
+        if(input==100)
+        {
+            system("adb shell input swipe 719 640 1 640");
+        }
+        if(input==43)
+        {
+            system("adb shell input keyevent VOLUME_UP");
+        }
+        if(input==45)
+        {
+            system("adb shell input keyevent VOLUME_DOWN");
 	}
         refresh();
     }
